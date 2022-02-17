00ccd81c9e03ec88a46626afd10afc30ff3a5d9b
javier.miralles <javimr2001@gmail.com>
2022-02-17 23:18:27 +0100

    Section 1 - Creating a proyect

---
+++ Scrumdinger/Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Javier Miralles Rancaño on 17/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
