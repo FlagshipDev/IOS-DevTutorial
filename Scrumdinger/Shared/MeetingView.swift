bd024db6193cd5ab4e9c17ae8727d9a12698d14f
javier.miralles <javimr2001@gmail.com>
2022-02-17 23:24:17 +0100

    Section 2 - Build groups of Views

---
+++ Scrumdinger/Shared/MeetingView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Javier Miralles Rancaño on 17/2/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
