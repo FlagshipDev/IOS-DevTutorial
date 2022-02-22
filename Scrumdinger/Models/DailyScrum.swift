0e9666b026711989d90ec88937ac25ff38068148
javier.miralles <javimr2001@gmail.com>
2022-02-22 23:45:25 +0100

    Finished "Passing Data with Binding"

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -33,13 +33,20 @@ extension DailyScrum {
     
     struct Data {
         var title: String = ""
-        var attendees: [Attendee] =  []
+        var attendees: [Attendee] = []
         var lengthInMinutes: Double = 5
         var theme: Theme = .seafoam
     }
     
     var data: Data {
         Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
+    }
+    
+    mutating func update(from data: Data) {
+        title = data.title
+        attendees = data.attendees
+        lengthInMinutes = Int(data.lengthInMinutes)
+        theme = data.theme
     }
 }
 
